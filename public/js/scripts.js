5bca5cc1b8dda0f2c7b79d7034b0cba681d01b2f
IvoBraatz <braatzivo@hotmail.com>
2024-07-16 20:28:39 -0300

    landing page complete

---
+++ public/js/scripts.js
@@ -0,0 +1,14 @@
+// Exemplo de script para adicionar funcionalidades dinâmicas, como rolagem suave
+
+document.querySelectorAll('.nav-links a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetSection.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
